68da0a0a673eb41a5a1089129504e7619ebe0206
raushanraj9 <raushanr363@gmail.com>
2024-01-09 15:05:50 +0530

    final app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import HomePage from "./pages/homePage/HomePage";
+import { UserContext } from "./context/userContext";
+import { PostsContext } from "./context/postsContext";
+import ProfilePage from "./pages/profilePage/ProfilePage";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  
+  // console.log("App runs")
+  const [userData, setUserData] = useState([]);
+  const [postsData, setPostsData] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/user/:id",
+      element: <ProfilePage />,
+    },
+  ]);
+
+  useEffect(() => {
+  // console.log("useEffect runs");
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((jsonData) => setUserData(jsonData));
+
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((jsonData) => setPostsData(jsonData));
+  }, []);
 
   return (
-    <>
-     <div>App component</div>
-        
-    </>
-  )
+    <div>
+      <UserContext.Provider value={userData}>
+        <PostsContext.Provider value={postsData}>
+            <RouterProvider router={router} />
+        </PostsContext.Provider>
+      </UserContext.Provider>
+    </div>
+  );
 }
 
-export default App
+export default App;
